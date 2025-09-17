4356852ab56c2515c503e85f04839b7cda5815ea
BenjjjO <benjoalex05@gmail.com>
2025-09-17 18:57:46 +0530

    Update f.js

--- f.js
+++ f.js
@@ -1,55 +1,58 @@
 const scrollers = document.querySelectorAll(".scroller");
-if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
-    addAnimation();
-}
-function addAnimation(){
-  scrollers.forEach((scroller)=>{
-    scroller.setAttribute("data-animated",true)
 
-    const scrollerInner = scroller.querySelector(".scroller__inner")
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller__inner");
     const scrollerContent = Array.from(scrollerInner.children);
-    scrollerContent.forEach((item)=>{
-const duplicatedItem = item.cloneNode(true)
-duplicatedItem.setAttribute("aria-hidden", true);
-scrollerInner.appendChild(duplicatedItem);
-    })
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
   });
 }
 
-const slides = document.querySelectorAll(".slide")
+const slides = document.querySelectorAll(".slide");
 let slideIndex = 0;
 let intervalId = null;
 
+document.addEventListener("DOMContentLoaded", initializeSlider);
 
-document.addEventListener("DOMContentLoaded",initializeSlider)
-
-function initializeSlider(){
-    if(slides.length > 0){
-        slides[slideIndex].classList.add("displaySlide")
-        intervalId = setInterval(nextslide,5000)
-    }
-
+function initializeSlider() {
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add("displaySlide");
+    intervalId = setInterval(nextSlide, 5000);
+  }
 }
 
-function showSlide(index){
-    if(index  >= slides.length){
-        slideIndex = 0;
-    }
-    else if(index < 0){
-        slideIndex = slides.length -1;
-    }
-slides.forEach(slide =>{
-    slide.classList.remove("displaySlide")
-    
-});
-slides[slideIndex].classList.add("displaySlide")
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  slides.forEach((slide) => {
+    slide.classList.remove("displaySlide");
+  });
+
+  slides[slideIndex].classList.add("displaySlide");
 }
-function prevslide(){
-    clearInterval(intervalId)
-    slideIndex--;
-    showSlide(slideIndex)
+
+function prevSlide() {
+  clearInterval(intervalId);
+  slideIndex--;
+  showSlide(slideIndex);
 }
-function nextslide(){
-slideIndex++;
-showSlide(slideIndex)
+
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
 }
